62f8e4c17e3a304891e633306c6a2b4d92c74fda
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-06-25 12:58:31 +0700

    finish

--- src/main/kotlin/16InheritanceF.kt
+++ src/main/kotlin/16InheritanceF.kt
@@ -1,6 +1,6 @@
 //Ketika kita meng-override property atau fungsi anggota kelas dari kelas induk, maka dari kelas
 //anak kita dapat mengakses property atau fungsi anggota dari kelas induknya. Caranya adalah
-//dengan menggunakan keyword super(), contohnya adalah seperti berikut :
+//dengan menggunakan keyword super(), contohnya adalah seperti berikut : 
 open class EmployeeF{
     open val baseSalery: Double = 10000.0
     open fun displayDetails() {
